c67e6c12aef98aedbdca41ebaf4f152248808093
Husky <Husky@Husky.dhcp.wustl.edu>
2022-09-07 16:58:01 -0500

    Studio 1

---
+++ src/studio1/IceCream.java
@@ -0,0 +1,16 @@
+package studio1;
+
+public class IceCream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The number of times you have been to Ted Drewes: int");
+		System.out.println("Whether you like Ted Drewes’ vanilla frozen custard or not: boolean");
+		System.out.println("The number of people you can take in your car: int");
+		System.out.println("The price of a regular size concrete vanilla frozen custard: double");
+		System.out.println("The name of your favorite flavor from Ted Drewes: String");
+		System.out.println("The address of the Ted Drewes: String");
+		System.out.println("The probability that you will see a friend at Ted Drewes: double");
+	}
+
+}
